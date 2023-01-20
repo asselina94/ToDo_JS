f2ea87e15141462f6d70b723fbd1fa40f8860fe0
Assel Batu <batuassel@gmail.com>
2023-01-20 14:11:21 +0100

    edit js

--- todo.js
+++ todo.js
@@ -4,7 +4,7 @@ function myFunction() {
   let x = document.getElementById("myText").value;
   todoItems.push(x);
 
-  document.getElementById("tasks").innerHTML = todoItems.join(",");
+  document.getElementById("tasks").innerHTML = todoItems.join("<br/>");
   
   console.log(todoItems);
   

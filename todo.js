45e8cfc0384f8d929e90f9ebd7af52b1e708a128
Assel Batu <batuassel@gmail.com>
2023-01-24 15:00:32 +0100

    backup.js

--- todo.js
+++ todo.js
@@ -1,13 +1,17 @@
 
 const todoItems = [];
-function myFunction() {
-  let x = document.getElementById("taskname").innerHTML += document.querySelector("#newtask input").value;
+function addTodo(){
+  let x = document.getElementById("myText").value;
   todoItems.push(x);
-
-  document.getElementById("taskname").innerHTML = todoItems.join("<br>")
-  
-  console.log(todoItems);
-  
 }
 
+function PrintTodo(){
+  let list = document.getElementById("tasks");
+  list.innerHTML = '';
+  var li = document.createElement('li');
+  for (const i = 0; i < todoItems.length; i++){
+    var listItem = $(document.createTextNode(todoItems[i]));
+    list.append(document.createTextNode(todoItems[i]));
 
+  }
+}

a5ead4f9d585613da1684c2c7d8eb06f1235f3ba
Assel Batu <batuassel@gmail.com>
2023-01-25 22:30:40 +0100

    Delete Function

--- backup.js
+++ backup.js
@@ -1,6 +1,12 @@
 
 const todoItems = [];
 
+function deleteTask(item){
+
+  let element = document.getElementById(item);
+  element.remove();
+ }
+
 function myFunction() {
   
   let x = document.getElementById("tasks").innerHTML = document.querySelector("#newtask input").value;
@@ -12,20 +18,25 @@ function myFunction() {
 
   for (let i = 0; i < todoItems.length; i++) {
     
-    data = data + `<li class="newtask">${todoItems[i]}</li>`;
-
+    data = data + `<li class="newtask">${todoItems[i]} 
+    <button class="delete" onclick="deleteTask(i)">
+    <i class="far fa-trash-alt"></i>
+    
+          </button></li>
+    `;
   }
   console.log(data);
   document.getElementById("tasks").innerHTML = data;
 
   
-  /*let item = document.createElement("li");
-  for (const i = 0; i < todoItems.length; i++){
-  let listitem = $(document.createTextNode(todoItems[i]));
-  list.append(document.createTextNode(todoItems[i]));
-  }*/
-  
-  console.log(todoItems);
+
+ /* let close = document.getElementsByClassName("delete");
+  for (let i; i < close.length; i++){
+    close[i].onclick = function() {
+      
+    }*/
+
+console.log(todoItems);
 }
 
 
